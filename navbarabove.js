b33fdb9de92fa2da3a8d70ffbab7949c69b251a1
Michael Robin <michaelrobin@Michaels-MacBook-Pro-3.local>
2013-10-27 17:08:46 -0500

    stuff

---
+++ navbarabove.js
@@ -0,0 +1,15 @@
+    enquire.register("screen and (min-width: 320px)", {
+        match: function() {
+        jQuery(document).ready(function () {
+    $('a[href^="#"]').click(function()
+    {
+        var sHref = this.href.split("#");
+        var topDistance = $("#"+sHref[1]).position().top;
+        var tds = topDistance -45;
+        $("html, body").animate({
+            scrollTop: tds
+        }, 'fast');
+    });
+});
+        }
+        });
